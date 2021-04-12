9b0b20c8627629e0496311d5056d210eb8c36c0e
Noah Ravetch <nravetch@calpoly.edu>
2021-04-12 14:59:40 -0700

    Partner modification

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World - Jack Engstrom - 2 Years at Cal Poly"); 
+        System.out.println("Hello World!");
     }
 }
